bb98ba87dad2133db392bb672a7ab06aa2f616ea
Andreas Aarsland <andreas@agens.no>
2013-10-25 08:33:37 +0200

    Moved to XCTest

--- Demo/AsyncTestWaitTests/AsyncTestWaitTests.h
+++ Demo/AsyncTestWaitTests/AsyncTestWaitTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Håvard Fossli. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface AsyncTestWaitTests : SenTestCase
+@interface AsyncTestWaitTests : XCTestCase
 
 @end
